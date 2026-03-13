ffd81d96ffbd94c34d6b31cdef30a2ce936ad8c0
Suraj Kumar Mahapatro <surajkumar@example.com>
2026-03-13 10:39:58 +0530

    Fix port binding for Render

--- app.py
+++ app.py
@@ -146,7 +146,9 @@ def manage():
     
     return render_template('manage.html', tasks=tasks)
 
+import os
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
 
